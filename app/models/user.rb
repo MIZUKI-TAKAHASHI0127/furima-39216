9f969fd16a268b7f84d3f01a4df692d4d5bb64ba
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-20 23:24:53 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
   validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
   validates :birthday, presence: true
