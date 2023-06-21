3ed50d7339094eef8ee489c709042995feb42917
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-21 20:22:20 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
-  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :birthday, presence: true
 
 
