960dee4d513654d5dd993822036b8a9b286393da
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-21 20:44:18 +0900

    パスワードバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+     
+
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
   validates :birthday, presence: true
+     
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'}
 
 
 end
