39d68ded0ba5e3206afc47575ddaba1fd7fcd2ef
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-28 17:43:42 +0900

    レビュー指摘点修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
 
   def index
