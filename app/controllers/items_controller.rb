153f7d68ac3f6b4dc50b1aac312fd5da0e2594e4
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-30 20:33:11 +0900

    レビュー指摘点修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -21,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
     if @item.user_id == current_user.id
     else
@@ -30,7 +29,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     # バリデーションがOKであれば詳細画面へ
     if @item.valid?
@@ -42,7 +40,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
  # def destroy
@@ -62,7 +59,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
   
-  #def set_item
-  #  @item = Item.find(params[:id])
- # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
